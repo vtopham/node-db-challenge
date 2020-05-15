cedfd611a5afe4995b671222623f8f1584a298a2
Victoria  Topham <topham.victoria@gmail.com>
2020-05-15 09:05:26 -0700

    updated getTasks to return the data as specified in the readme

--- api/pm_model.js
+++ api/pm_model.js
@@ -30,7 +30,12 @@ function addTask (task) {
         .insert(task)
 }
 function getTasks () {
-    return db.select('*')
+    return db.select(db.ref('Projects.name').as('Project Name'), 
+    db.ref('Projects.description').as('Project Description'), 
+    db.ref('Tasks.id').as("Task Id"), 
+    db.ref('Tasks.description').as("Task Description"), 
+    db.ref('Tasks.notes').as("Task Notes"), 'Tasks.completed_status')
         .from('Tasks')
+        .join('Projects', 'Projects.id', '=', 'Tasks.project_id')
 
 }
